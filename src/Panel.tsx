4195ba79a30e246911a941cd330ac82967c04ffa
Ari Perkkiö <ari.perkkio@gmail.com>
2022-01-28 17:58:45 +0000

    feat: style announcement logs

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useAddonState, useChannel } from '@storybook/api';
 import { AddonPanel } from '@storybook/components';
 import { STORY_CHANGED } from '@storybook/core-events';
+import { styled, themes, convert } from '@storybook/theming';
 import type { PolitenessSetting } from 'aria-live-capture';
 
 import { ADDON_ID, EVENTS } from './constants';
@@ -10,6 +11,25 @@ interface Announcement {
   textContent: string;
   politenessSetting: PolitenessSetting;
 }
+
+const List = styled.ol`
+  list-style: none;
+  margin: 0;
+  padding: 1rem;
+`;
+
+const ListItem = styled.li`
+  font-size: 14px;
+  margin: 0.5rem 0;
+`;
+
+const PolitenessLabel = styled.span<Announcement>`
+  font-weight: 600;
+  text-transform: uppercase;
+  padding-right: 0.5rem;
+  color: ${(props) =>
+    props.politenessSetting === 'assertive' ? 'red' : 'blue'};
+`;
 
 export const Panel: React.FC<{ active: boolean }> = (props) => {
   const [captures, setCaptures] = useAddonState<Announcement[]>(ADDON_ID, []);
@@ -26,13 +46,16 @@ export const Panel: React.FC<{ active: boolean }> = (props) => {
 
   return (
     <AddonPanel {...props}>
-      <ol>
+      <List>
         {captures.map((capture, index) => (
-          <li key={index}>
-            [{capture.politenessSetting.toUpperCase()}] {capture.textContent}
-          </li>
+          <ListItem key={index}>
+            <PolitenessLabel {...capture}>
+              [{capture.politenessSetting}]
+            </PolitenessLabel>{' '}
+            {capture.textContent}
+          </ListItem>
         ))}
-      </ol>
+      </List>
     </AddonPanel>
   );
 };
