2194142f518468f85199c39cb00f78e88df2d7f9
Ari Perkkiö <ari.perkkio@gmail.com>
2024-05-12 16:02:56 +0300

    chore(deps): update `aria-live-capture`, remove work-around

--- src/withCaptures.ts
+++ src/withCaptures.ts
@@ -1,11 +1,8 @@
+import CaptureAnnouncements from 'aria-live-capture';
 import { useEffect, useChannel, useParameter } from '@storybook/preview-api';
 import { PartialStoryFn as StoryFunction } from '@storybook/types';
 
 import { EVENTS } from './constants';
-
-// TODO: aria-live-capture should publish proper ESM build
-import * as AriaLiveCapture from 'aria-live-capture';
-const CaptureAnnouncements = unwrapPoorlyPublishedPackage(AriaLiveCapture);
 
 export function withCaptures(storyFn: StoryFunction) {
   const { includeShadowDom } = useParameter('aria-live', {
@@ -27,12 +24,4 @@ export function withCaptures(storyFn: StoryFunction) {
 
   // @ts-ignore TODO check issues
   return storyFn();
-}
-
-// Unwrap possible double-wrapped CJS package
-function unwrapPoorlyPublishedPackage<T extends { default: unknown }>(
-  pkg: T
-): T['default'] {
-  // @ts-expect-error
-  return pkg.default?.default || pkg.default;
 }
